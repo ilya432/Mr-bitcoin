0a4e3600c1c16dacae533487fe756716101d054d
Ilya Levin <ilya432@gmail.com>
2020-01-29 03:26:55 +0200

    after build3

---
+++ public/precache-manifest.3d04164cdf428a64da9de20a589087a5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f6e6c0da509691aba0731c57d197053",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b7cc883017f2a90e045",
+    "url": "/static/css/main.91e86bdc.chunk.css"
+  },
+  {
+    "revision": "354b143fbb03efb3cece",
+    "url": "/static/js/2.6a359b56.chunk.js"
+  },
+  {
+    "revision": "138a6d51240ab070e8d1b20c54be6394",
+    "url": "/static/js/2.6a359b56.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b7cc883017f2a90e045",
+    "url": "/static/js/main.35da214e.chunk.js"
+  },
+  {
+    "revision": "c9f1875f7e246a67df7e",
+    "url": "/static/js/runtime-main.cb82405f.js"
+  },
+  {
+    "revision": "4cd3bf5d0ae6e6fdd8ac123de9e5450d",
+    "url": "/static/media/bitcoin-small.4cd3bf5d.png"
+  },
+  {
+    "revision": "6af14d7cc1fb24245a16603db50c0b9d",
+    "url": "/static/media/login.6af14d7c.png"
+  }
+]);
